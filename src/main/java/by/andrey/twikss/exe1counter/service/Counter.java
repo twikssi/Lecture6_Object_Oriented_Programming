b04638ccaac8c8f4bf423fe676a6360824146b26
twikssi <limpbiski15@gmail.com>
2020-09-02 17:38:15 +0300

    created getter and setter with conditional for field step

--- src/main/java/by/andrey/twikss/exe1counter/service/Counter.java
+++ src/main/java/by/andrey/twikss/exe1counter/service/Counter.java
@@ -7,8 +7,11 @@ public class Counter {
     private Logger logger = LoggerFactory.getLogger(Counter.class);
     private int value;
     private int step;
-    private final int MAX_COUNTER = 100;
-    private final int MIN_COUNTER = 0;
+    private static final int MAX_COUNTER = 100;
+    private static final int MIN_COUNTER = 0;
+    private static final int MAX_STEP = 10;
+    private static final int MIN_STEP = 1;
+
 
     public Counter() {
         this.value = 0;
@@ -19,12 +22,25 @@ public class Counter {
         return value;
     }
 
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        if (step >= MIN_STEP && step <= MAX_STEP){
+            this.step = step;
+        } else {
+            logger.info("Value step Counter can be between (1 - 10)");
+        }
+    }
+
     public void setValue(int value) {
         if (value >= MIN_COUNTER && value <= MAX_COUNTER){
             this.value = value;
         } else {
             logger.info("Value Counter can be between (0 - 100)");
         }
+
 
     }
 }
