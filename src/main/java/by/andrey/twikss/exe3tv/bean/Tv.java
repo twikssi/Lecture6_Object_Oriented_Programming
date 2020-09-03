89acb38b819b8e8661d8549f983cc2e6506e864c
twikssi <limpbiski15@gmail.com>
2020-09-03 16:17:55 +0300

    created class Tv

---
+++ src/main/java/by/andrey/twikss/exe3tv/bean/Tv.java
@@ -0,0 +1,8 @@
+package by.andrey.twikss.exe3tv.bean;
+
+public class Tv {
+    private int currentChanal;
+    private int currentVolume;
+    private String company;
+    private boolean switchTV = false;
+}
