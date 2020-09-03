55c1b8a82e9a7104ec5fbdf273e1dc68f4adcd77
twikssi <limpbiski15@gmail.com>
2020-09-03 16:25:50 +0300

    added getters and toString, hashcode, equals to method Tv

--- src/main/java/by/andrey/twikss/exe3tv/bean/Tv.java
+++ src/main/java/by/andrey/twikss/exe3tv/bean/Tv.java
@@ -1,8 +1,58 @@
 package by.andrey.twikss.exe3tv.bean;
 
+import java.util.Objects;
+
 public class Tv {
     private int currentChanal;
     private int currentVolume;
     private String company;
     private boolean switchTV = false;
+
+    public Tv(int currentChanal, int currentVolume, String company) {
+        this.currentChanal = currentChanal;
+        this.currentVolume = currentVolume;
+        this.company = company;
+    }
+
+    public int getCurrentChanal() {
+        return currentChanal;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public boolean isSwitchTV() {
+        return switchTV;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tv tv = (Tv) o;
+        return currentChanal == tv.currentChanal &&
+                currentVolume == tv.currentVolume &&
+                switchTV == tv.switchTV &&
+                Objects.equals(company, tv.company);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currentChanal, currentVolume, company, switchTV);
+    }
+
+    @Override
+    public String toString() {
+        return "Tv{" +
+                "currentChanal=" + currentChanal +
+                ", currentVolume=" + currentVolume +
+                ", company='" + company + '\'' +
+                ", switchTV=" + switchTV +
+                '}';
+    }
 }
